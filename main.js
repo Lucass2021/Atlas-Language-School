bd1449618aa69ae2bc65039d3ac1336db199382a
Lucas Dias <lucas.allx@hotmail.com>
2021-06-09 08:03:45 -0300

    testimonial commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// =====Banner Slider =====
+
 let time = 3100,
     currentImageIndex = 0,
     images = document.querySelectorAll(".slider img")
@@ -26,3 +28,43 @@ function changeBanner() {
 }
 
 window.addEventListener("load", changeBanner);
+
+
+// =====Testimonial Slider =====
+
+const btn = document.getElementsByClassName("btn");
+const slide = document.getElementById("slide");
+
+btn[0].onclick = function () {
+    slide.style.transform = "translateX(0px)";
+    for (i = 0; i < 4; i++) {
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+
+}
+
+btn[1].onclick = function () {
+    slide.style.transform = "translateX(-800px)";
+    for (i = 0; i < 4; i++) {
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+
+}
+btn[2].onclick = function () {
+    slide.style.transform = "translateX(-1600px)";
+    for (i = 0; i < 4; i++) {
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
+btn[3].onclick = function () {
+    slide.style.transform = "translateX(-2400px)";
+    for (i = 0; i < 4; i++) {
+        btn[i].classList.remove("active");
+    }
+    this.classList.add("active");
+}
+
