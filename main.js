9f399c70f1c5c4b38cc6cbd90bb27b4597bdf119
Lucas Dias <lucas.allx@hotmail.com>
2021-06-13 11:16:07 -0300

    first page completed

--- main.js
+++ main.js
@@ -30,42 +30,19 @@ function changeBanner() {
 window.addEventListener("load", changeBanner);
 
 
-// =====Testimonial Slider  Buttons =====
 
-const btn = document.getElementsByClassName("btn");
-const slide = document.getElementById("slide");
+// =====Testimonial Own =====
 
-btn[0].onclick = function () {
-    slide.style.transform = "translateX(0px)";
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active");
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    autoplayHoverPause: false,
+    nav: true,
+    margin: 350,
+    responsive: {
+        0: {
+            items: 1
+        }
     }
-    this.classList.add("active");
-
-}
-
-btn[1].onclick = function () {
-    slide.style.transform = "translateX(-800px)";
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active");
-    }
-    this.classList.add("active");
-
-}
-btn[2].onclick = function () {
-    slide.style.transform = "translateX(-1600px)";
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active");
-    }
-    this.classList.add("active");
-}
-
-btn[3].onclick = function () {
-    slide.style.transform = "translateX(-2400px)";
-    for (i = 0; i < 4; i++) {
-        btn[i].classList.remove("active");
-    }
-    this.classList.add("active");
-}
-
-// =====Testimonial Slider Arrow =====
+})
